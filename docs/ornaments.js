afddcfa75263850ac0cba5a298bc7230479654be
Louis-VAK <luis961156@gmail.com>
2025-12-09 14:38:43 +0800

    Update ornaments.js

--- docs/ornaments.js
+++ docs/ornaments.js
@@ -1,54 +1,48 @@
 // ornaments.js
 export async function createOrnaments(canvas) {
-    const ctx = canvas.getContext("2d");
+  const imgPaths = [
+    "./assets/img1.png",
+    "./assets/img2.png",
+    "./assets/img3.png",
+    "./assets/img4.png",
+    "./assets/img5.png",
+    "./assets/img6.png",
+  ];
 
-    const imgPaths = [
-        "./assets/img1.png",
-        "./assets/img2.png",
-        "./assets/img3.png",
-        "./assets/img4.png",
-        "./assets/img5.png",
-        "./assets/img6.png"
-    ];
+  const images = await Promise.all(imgPaths.map(loadImage));
 
-    const images = await Promise.all(
-        imgPaths.map(src => loadImage(src))
-    );
+  const ornaments = images.map((img, i) => {
+    return {
+      img,
+      x: canvas.width / 2 + (i % 2 === 0 ? -220 : 220),
+      y: canvas.height / 2 - 150 + i * 80,
+      baseSize: 120,
+    };
+  });
 
-    const ornaments = images.map((img, i) => {
-        return {
-            img,
-            x: canvas.width / 2 + Math.cos(i) * 200,
-            y: canvas.height / 2 + Math.sin(i) * 200,
-            size: 120,
-            baseSize: 120
-        };
-    });
-
-    return ornaments;
+  return ornaments;
 }
 
 export function updateOrnaments(ctx, ornaments, handPos) {
-    ornaments.forEach(o => {
-        let scale = 1;
+  ornaments.forEach(o => {
+    let scale = 1;
 
-        if (handPos) {
-            const dx = o.x - handPos.x;
-            const dy = o.y - handPos.y;
-            const dist = Math.sqrt(dx * dx + dy * dy);
+    if (handPos) {
+      const dx = o.x - handPos.x;
+      const dy = o.y - handPos.y;
+      const dist = Math.sqrt(dx * dx + dy * dy);
+      if (dist < 180) scale = 1.4;
+    }
 
-            if (dist < 150) scale = 1.5;
-        }
-
-        const finalSize = o.baseSize * scale;
-        ctx.drawImage(o.img, o.x - finalSize / 2, o.y - finalSize / 2, finalSize, finalSize);
-    });
+    const size = o.baseSize * scale;
+    ctx.drawImage(o.img, o.x - size / 2, o.y - size / 2, size, size);
+  });
 }
 
 function loadImage(src) {
-    return new Promise(res => {
-        const img = new Image();
-        img.onload = () => res(img);
-        img.src = src;
-    });
+  return new Promise(res => {
+    const img = new Image();
+    img.onload = () => res(img);
+    img.src = src;
+  });
 }
